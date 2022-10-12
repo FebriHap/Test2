4ad2bd008c270c3af22173f1576f10617cad8f5a
FebriHap <114375574+FebriHap@users.noreply.github.com>
2022-10-12 15:20:19 +0700

    test

--- app/src/main/java/com/if3b/test2/MainActivity.java
+++ app/src/main/java/com/if3b/test2/MainActivity.java
@@ -3,12 +3,42 @@ package com.if3b.test2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.if3b.test2.R;
 
 public class MainActivity extends AppCompatActivity {
+    Button btntekan;
+    EditText etnama;
+    TextView tvnama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Uji Coba");
+
+        btntekan =findViewById(R.id.btntekan);
+        etnama = findViewById(R.id.etnama);
+        tvnama = findViewById(R.id.tvnama);
+
+        btntekan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nama = etnama.getText().toString();
+                String hasil = tvnama.getText().toString();
+
+                if (nama.trim().equals("")){
+                    etnama.setError("Harus Diisi");
+                    return;
+                }
+
+
+            }
+        });
     }
 }
